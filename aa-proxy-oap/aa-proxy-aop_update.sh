be7557dca095ef70b7c250d944176504ed0b3090
George G <kreach3r@gmail.com>
2025-01-03 00:51:46 +0200

    navipi-usb-update: Start configuring setup
    to be compatible with my installation solution.

--- aa-proxy-oap/aa-proxy-aop_update.sh
+++ aa-proxy-oap/aa-proxy-aop_update.sh
@@ -1,1 +1,6 @@
-placeholder fow now
+function services_update {
+    echo "Enabling services"
+    # Include here all the necessary changes to services
+    sudo systemctl enable aa-proxy.service
+    sudo systemctl enable aa-usbgadget.service
+}
